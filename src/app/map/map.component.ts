a3ae35387e1fdc04598a9a882d270f1a462b05ce
littleFootDev <thelittlefootdev@gmail.com>
2022-10-26 20:05:40 +0200

    Get my position

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet'
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+  
 
-  constructor() { }
+  private initMap(): void {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const map = L.map('my-map', {
+        center: [position.coords.latitude, position.coords.longitude],
+        zoom:3
+      });
 
-  ngOnInit(): void {
+      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        minZoom: 1,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      });
+      tiles.addTo(map)
+
+      L.marker([position.coords.latitude, position.coords.longitude])
+        .addTo(map)
+        .bindPopup('My position')
+        
+    })
+
+
+
+    
   }
-
+  ngAfterViewInit(): void {
+    this.initMap()
+  }
 }
